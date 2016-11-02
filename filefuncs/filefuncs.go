180c5b6cb432d8cc6409b233fb731b7c0646dac1
DonMills <dmills@singlestoneconsulting.com>
2016-11-02 15:38:40 -0400

    Fixing base64 padding issue

--- filefuncs/filefuncs.go
+++ filefuncs/filefuncs.go
@@ -7,23 +7,24 @@ import (
 
 var sep = []byte{0, 1, 0, 1, 0, 1}
 
+//CreateEncFile takes the key, iv, and encrypted data and concatenates it to a file
 func CreateEncFile(ciphertext []byte, iv []byte, cipherdatakey []byte) []byte {
 	bufferslice := [][]byte{cipherdatakey, iv, ciphertext}
 	concat := bytes.Join(bufferslice, sep)
-	encodelen := base64.StdEncoding.EncodedLen(len(concat))
+	encodelen := base64.RawStdEncoding.EncodedLen(len(concat))
 	encdata := make([]byte, encodelen)
-	base64.StdEncoding.Encode(encdata, concat)
+	base64.RawStdEncoding.Encode(encdata, concat)
 	return encdata
 }
 
+//SplitEncFile takes the concatenated file and splits out the key, iv, and data
 func SplitEncFile(filedata []byte) ([]byte, []byte, []byte) {
-	decodelen := base64.StdEncoding.DecodedLen(len(filedata))
+	decodelen := base64.RawStdEncoding.DecodedLen(len(filedata))
 	decodeddata := make([]byte, decodelen)
-	base64.StdEncoding.Decode(decodeddata, filedata)
+	base64.RawStdEncoding.Decode(decodeddata, filedata)
 	returnslice := bytes.SplitN(decodeddata, sep, 3)
 	key := returnslice[0]
 	iv := returnslice[1]
-	suffix := []byte{0}
-	data := bytes.TrimSuffix(returnslice[2], suffix)
+	data := returnslice[2]
 	return data, iv, key
 }
